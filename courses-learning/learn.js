6194da61d36caa67bded65d8aeb722ea69119cad
shegilo <ilorioluwasegun@gmail.com>
2020-06-20 21:10:45 +0100

    Initial Commit

---
+++ courses-learning/learn.js
@@ -0,0 +1,135 @@
+var arrr;
+var fawelioke = ["á", "Á", "é", "É", "ẹ́", "Ẹ́", "í", "Í", "ó", "Ó", "ọ́", "Ọ́", "ú", "Ú", "á", "Á", "é", "É", "ẹ́", "Ẹ́", 
+                    "í", "Í" , "ó", "Ó", "ọ́", "Ọ́", "ú", "Ú"];
+
+var faweliarin = ["a", "A", "e", "E", "ẹ", "Ẹ", "I" ,"I", "o", "O." ,"ọ", "Ọ", "u", "U",
+                    "a", "A", "e", "ẹ", "E", "Ẹ", "i", "I",  "o", "ọ", "O", "Ọ", "u", "U"];
+
+var fawelisale = ["à", "À", "è", "È", "ẹ̀", "Ẹ̀", "ì", "Ì", "ò", "Ò", "ọ̀", "Ọ̀", "ù", "Ù", "à", "À", "è",
+                        "È" ,"ẹ̀", "Ẹ̀", "ì", "Ì", "ò", "Ò", "ọ̀", "Ọ̀", "ù", "Ù"];
+
+var nasalisale = ["ǹ", "Ǹ", "m̀", "M̀", "ǹ", "Ǹ", "m̀", "M̀"];
+
+var nasaloke = ["ń", "Ń", "ḿ", "Ḿ", "ń", "Ń", "ḿ", "Ḿ"];
+
+var soundstoplay = [];
+
+
+function splitString() {
+    var string = document.getElementById('search-input').value;
+
+    if(string.length >0){
+    var splitters = ["gb","GB","b","B","d","D","f","F","g","G","j","J","k","K","l","L","m","n","N","M","p","P","r","R","s","S","ṣ","Ṣ","t","T",
+    "w","W","y","Y"];
+    var list = [string];
+    for(var i=0, len=splitters.length; i<len; i++) {
+        traverseList(list, splitters[i], 0);
+    }
+     arrr = (flatten(list));
+    document.getElementById('playtext').innerHTML = "Click to play Sound";
+    document.getElementById('playhide').style.display="inline";
+    console.log(arrr);
+}else{
+    alert('Invalid word');
+}
+}
+
+traverseList = function(list, splitter, index) {
+    if(list[index]) {
+        if((list.constructor !== String) && (list[index].constructor === String))
+            (list[index] != list[index].split(splitter)) ? list[index] = list[index].split(splitter) : null;
+        (list[index].constructor === Array) ? traverseList(list[index], splitter, 0) : null;
+        (list.constructor === Array) ? traverseList(list, splitter, index+1) : null;    
+    }
+}
+
+flatten = function(arr) {
+    return arr.reduce(function(acc, val) {
+        return acc.concat(val.constructor === Array ? flatten(val) : val);
+    },[]);
+}
+
+
+function checkTone(splitted, number){
+    number = parseInt(number);
+
+    if(fawelioke.includes(splitted)){
+
+        soundstoplay[number] = "audio/Mih.mp3";
+   
+    }    
+    else if(fawelisale.includes(splitted)){
+
+        soundstoplay[number] = "audio/Doh.mp3";
+   
+    }
+    else if(faweliarin.includes(splitted)){
+
+        soundstoplay[number] = "audio/Reh.mp3";
+
+    }
+    else if(nasalisale.includes(splitted)){
+
+        soundstoplay[number] = "audio/Doh.mp3";
+   
+
+    }
+    else if(nasaloke.includes(splitted)){
+
+        soundstoplay[number] = "audio/Mih.mp3";
+   
+    }
+
+}
+
+function playSound(){
+    for (i = 0; i < arrr.length; i++) {
+    
+        if(arrr[i].length >0){
+                 console.log(arrr[i]);
+                checkTone(arrr[i], i);
+    
+                //Do some stuff here
+             }
+        else{
+            soundstoplay[i] = "null";
+        }
+    
+    }
+
+    console.log(soundstoplay);
+    playAllSOund();
+} 
+
+
+function myFunction() {
+    document.getElementById('playtext').innerHTML = "";
+    document.getElementById('playhide').style.display="none";
+ 
+}
+function playAllSOund(){
+    var start = true;
+    var audio = new Audio(),
+    i = 0;
+    var playlist = soundstoplay;
+
+audio.addEventListener('ended', function () {
+  if(start){
+    i = ++i < playlist.length ? i : 0;
+    console.log(i);
+    audio.src = playlist[i];
+    audio.play();
+  if(i== playlist.length-1){
+      start = false;
+  }  
+}
+}, true);
+
+
+audio.volume = 0.5;
+audio.loop = false;
+audio.src = playlist[0];
+audio.play();
+
+}
+  
